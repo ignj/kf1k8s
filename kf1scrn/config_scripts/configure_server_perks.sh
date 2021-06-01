f3814f199e4edc6a0b5ac971e33ae5ce9c583dd0
ignj <ignacio.jocano@gmail.com>
2021-06-01 18:25:17 -0300

    customize scrn image

---
+++ kf1scrn/config_scripts/configure_server_perks.sh
@@ -0,0 +1,3 @@
+declare -A configuration_map
+
+sed -i "s/MinPerksLevel=0/MinPerksLevel=6/g" ServerPerks.ini
--- kf1scrn/main.sh
+++ kf1scrn/main.sh
@@ -9,6 +9,9 @@ docker build \
     --build-arg STEAM_PASSWORD="$steam_password" \
     --build-arg SCRN_GDRIVE_ID="$scrn_gdrive_id" \
     --build-arg SCRN_GDRIVE_FILE_NAME="$scrn_gdrive_file_name" \
+    --build-arg GAME_PORT="$game_port" \
+    --build-arg QUERY_PORT="$query_port" \
+    --build-arg LISTEN_PORT="$listen_port" \
     --build-arg GAME_ADMIN_NAME="$game_admin_name" \
     --build-arg GAME_ADMIN_PASSWORD="$game_admin_password" \
     --build-arg SERVERNAME="$server_name" \
